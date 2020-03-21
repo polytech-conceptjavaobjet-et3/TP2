344dd78639339655ed27ae9cb57de978d33991a1
Eugénie Brasier <eugenie.brasier@gmail.com>
2020-03-21 12:16:27 +0100

    m

---
+++ src/test/Utilisateur.java
@@ -0,0 +1,12 @@
+package test;
+
+import et3.java.TableauEntiers;
+
+public class Utilisateur
+{
+	public static void main(String[] args)
+	{
+		int[] tabInt = {4, 5, 9};
+		TableauEntiers.afficherTableau(tabInt);
+	}
+}
